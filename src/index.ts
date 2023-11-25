9dd345225a479a295b92456a2e9f4b281610a99d
Denis K <wolf-den@tut.by>
2023-11-25 15:41:06 +0300

    feat: hot/cold

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-// import { Observable } from "rxjs";
+import { Observable } from "rxjs";
 
-// const sequence$ = new Observable((subscriber) => {
-//   let count = 1;
-//   const intervalId = setInterval(() => {
-//     if (count % 5 === 0) {
-//       clearInterval(intervalId);
-//       subscriber.complete();
-//       return
-//     }
-//     // subscriber.error('test error');
-//     subscriber.next(count++);
-//   }, 1000)
+const socket: WebSocket = new WebSocket('wss://echo.websocket.org');
+const sequence$ = new Observable((subscriber) => {
+  socket.addEventListener('message', (e) => {
+    subscriber.next(e)
+  })
 
-//   return () => {
-//     console.log('unsubscribe');
-//     clearInterval(intervalId);
-//   }
-// })
+  // let count = 1;
+  // const intervalId = setInterval(() => {
+  //   // if (count % 5 === 0) {
+  //   //   clearInterval(intervalId);
+  //   //   subscriber.complete();
+  //   //   return
+  //   // }
+  //   // subscriber.error('test error');
+  //   subscriber.next(count++);
+  // }, 1000)
+
+  // return () => {
+  //   console.log('unsubscribe');
+  //   clearInterval(intervalId);
+  // }
+})
 
 // const subscription = sequence$.subscribe(
 //   (v) => {
@@ -34,18 +39,42 @@
 //   subscription.unsubscribe();
 // }, 3000)
 
-import { interval } from "rxjs";
+/**
+ * Cold
+ */
+// import { interval } from "rxjs";
 
-const sequence$ = interval(1000);
+// const sequence$ = interval(1000);
+
+// const sub1 = sequence$.subscribe((v) => {
+//   console.log('Sub 1', v);
+// })
+
+// setTimeout(() => {
+//   sequence$.subscribe((v) => {
+//     console.log('Sub 2', v);
+//   })
+// }, 5000)
+
+/**
+ * Hot
+ */
+// import { fromEvent } from "rxjs";
+
+// const sequence$ = fromEvent<MouseEvent>(document, 'click');
 
 const sub1 = sequence$.subscribe((v) => {
   console.log('Sub 1', v);
 })
-
-const sub2 = sequence$.subscribe((v) => {
-  console.log('Sub 2', v);
+socket.addEventListener('open', () => {
+  let count = 1;
+  const intervalId = setInterval(() => {
+    socket.send((count++).toString())
+  }, 1000)
 })
 
 setTimeout(() => {
-  sub1.unsubscribe();
-}, 3000)
+  sequence$.subscribe((v) => {
+    console.log('Sub 2', v);
+  })
+}, 5000)
